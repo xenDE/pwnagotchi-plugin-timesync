9b8a6ea6f91b6269c24199509c237312b332cf10
xenDE <danniel@mameso.com>
2019-10-19 18:00:20 +0100

    little clenup

--- timesync.py
+++ timesync.py
@@ -24,24 +24,32 @@ class RaspiTimeSync:
                 random_seed_data = f.read()
             f.close() 
             self._boot_uuid = hashlib.md5(random_seed_data).hexdigest();
-
             self._uuid_json_file = "/var/local/time-sync/" + self._boot_uuid + ".json"
-            with open(self._uuid_json_file, 'r') as json_file:
-                self._json = json.load(json_file)
         except json.JSONDecodeError as js_e:
             raise js_e
+        self._json = self.getJsonDict(self._uuid_json_file)
         self._boot_uptime = self._json['boot_uptime']
         self._boot_ts = self._json['boot_ts']
         self._is_synced = self._json['synced']
         self.checkSync()
 
+    def getJsonDict(self, file_name):
+        try:
+            with open(file_name, 'r') as json_file:
+                return json.load(json_file)
+        except json.JSONDecodeError as js_e:
+            raise js_e
+
+    def getUptime(self):
+        with open("/proc/uptime","r") as f:
+            uptime = f.read()
+        f.close()
+        return int("%.0f" % float(uptime.split(' ')[0]))
+        
     def checkSync(self):
         if self._is_synced == 0:
+            now_uptime = self.getUptime()
             now_ts = int("%.0f" % time.time())
-            with open("/proc/uptime","r") as f:
-                uptime = f.read()
-            f.close()
-            now_uptime = int("%.0f" % float(uptime.split(' ')[0]))
             boot_offset = self._boot_ts - self._boot_uptime
             now_offset = now_ts - now_uptime
             self._offset = now_offset - boot_offset
@@ -56,8 +64,19 @@ class RaspiTimeSync:
                         json.dump(self._json, uuid_json_file)
                 except OSError as os_e:
                     logging.error("TIME-SYNC: %s", os_e)
+                self._is_synced = 1
 
+    def getTime(self):
+        time = []
+        time['boot_uuid'] = self._boot_uuid
+        time['boot_uptime'] = self.getUptime()
+        time['timestamp'] = int("%.0f" % time.time())
+        time['synced'] = self._is_synced
+        return time
 
+    def getTimeCorrected(self, time):
+        time_return = []
+        return time_return
 
 raspi_time_sync = RaspiTimeSync()
 
