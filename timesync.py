ae8769cecd62b9583445cf2b88f1a4f72565f6cb
xenDE <daniel@mameso.com>
2019-10-21 23:54:59 +0100

    some cleanup - time for a python nerd to make this clean and usable in pwnagotchi

--- timesync.py
+++ timesync.py
@@ -18,66 +18,95 @@ class RaspiTimeSync:
     """
     # es sollte nur eine instnz dieser klasse existieren dürfen
 
+    # if time is synced, the time DictObj is shorter: {"ts": 0, "synced" 1}
+    time = {"ts": 0, "synced": 0, "boot_uuid": "", "boot_uptime": 0, "boot_ts": 0, "sync": {"offset": 0, "uptime": 0}}
+    _is_synced = 0
+
     def __init__(self):
         try:
-            with open("/var/lib/systemd/random-seed","rb") as f:
-                random_seed_data = f.read()
-            f.close() 
-            self._boot_uuid = hashlib.md5(random_seed_data).hexdigest();
-            self._uuid_json_file = "/var/local/time-sync/" + self._boot_uuid + ".json"
-        except json.JSONDecodeError as js_e:
-            raise js_e
-        self._json = self.getJsonDict(self._uuid_json_file)
-        self._boot_uptime = self._json['boot_uptime']
-        self._boot_ts = self._json['boot_ts']
-        self._is_synced = self._json['synced']
-        self.checkSync()
+          with open("/var/lib/systemd/random-seed","rb") as f:
+            random_seed_data = f.read()
+          f.close() 
+          self.time["boot_uuid"] = hashlib.md5(random_seed_data).hexdigest();
+        except Exception as error:
+          print("error on reading /var/lib/systemd/random-seed: ", error)
+        json_boot = self._getJsonBootDict(self.time["boot_uuid"])
+        self.time = {**self.time, **json_boot}
+        self._is_synced = self.time["synced"]
+        if self._is_synced == 0:
+          self._checkSync()
+        else:
+          self.time = {"ts": 0, "synced": 1}    # make timeDict shorter
 
-    def getJsonDict(self, file_name):
+    def getTime(self, intime = None):
+      if intime is None:
+        intime = self.time
+        intime["ts"] = int("%.0f" % time.time())
+        return intime
+      else:
+        if intime["synced"] == 1:
+          return intime
+        else:
+          return self._checkSync(intime)
+
+    def _getJsonBootDict(self, boot_uuid):
+        file_name = "/var/local/time-sync/" + boot_uuid + ".json"
         try:
             with open(file_name, 'r') as json_file:
                 return json.load(json_file)
         except json.JSONDecodeError as js_e:
             raise js_e
 
-    def getUptime(self):
+    def _getUptime(self):
         with open("/proc/uptime","r") as f:
             uptime = f.read()
         f.close()
         return int("%.0f" % float(uptime.split(' ')[0]))
-        
-    def checkSync(self):
-        if self._is_synced == 0:
-            now_uptime = self.getUptime()
-            now_ts = int("%.0f" % time.time())
-            boot_offset = self._boot_ts - self._boot_uptime
-            now_offset = now_ts - now_uptime
-            self._offset = now_offset - boot_offset
-#            print("offset: " + str(self._offset))
-            # wenn zeit differenz über 60 sekunden
-            if abs(self._offset) > 60:
-                self._json['synced'] = 1
-                self._json['sync']['uptime'] = now_uptime
-                self._json['sync']['offset'] = self._offset
-                try:
-                    with open(self._uuid_json_file, 'w+t') as uuid_json_file:
-                        json.dump(self._json, uuid_json_file)
-                except OSError as os_e:
-                    logging.error("TIME-SYNC: %s", os_e)
-                self._is_synced = 1
 
-    def getTime(self):
-        time = []
-        time['boot_uuid'] = self._boot_uuid
-        time['boot_uptime'] = self.getUptime()
-        time['timestamp'] = int("%.0f" % time.time())
-        time['synced'] = self._is_synced
-        return time
+    def _checkSync(self, intime=None):
+        if intime is None:
+          check_time = self.time
+          if check_time["synced"] == 1:
+            return check_time
+          uptime = self._getUptime()
+          boot_offset = self.time["boot_ts"] - self.time["boot_uptime"]
+          now_ts = int("%.0f" % check_time.time())
+          now_offset = now_ts - uptime
+          offset = now_offset - boot_offset
+        else:
+          check_time = intime
+          if check_time["synced"] == 1:
+            return check_time
+          boot_time = self._getJsonBootDict(check_time["boot_uuid"])
+          if boot_time["synced"] == 1:
+            offset = boot_time["sync"]["offset"]
+          else:
+            return check_time
+          uptime = boot_time["boot_uptime"]
+          boot_offset = boot_time["boot_ts"] - self.time["boot_uptime"]
 
-    def getTimeCorrected(self, time):
-        time_return = []
-        return time_return
+#        print("new offset: " + str(offset))
+
+        # wenn zeit differenz über 60 sekunden
+        if abs(offset) > 60:
+          synced_time = {"ts": check_time + offset, "synced": 1}
+
+        # save calculated offset, if this boot
+        if intime is None:
+          save_time = check_time
+          save_time["synced"] = 1
+          save_time["sync"]["offset"] = offset
+          save_time["sync"]["uptime"] = uptime
+          try:
+            file_name = "/var/local/time-sync/" + save_time["boot_uuid"] + ".json"
+            with open(file_name, 'w+t') as uuid_json_file:
+              json.dump(save_time, uuid_json_file)
+          except OSError as os_e:
+            logging.error("TIME-SYNC: %s", os_e)
+          self.time = save_time
+
 
 raspi_time_sync = RaspiTimeSync()
 
+print(raspi_time_sync.getTime())
 
